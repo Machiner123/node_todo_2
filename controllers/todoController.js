e193dd6e99b62f53877fcadfc0f62d82c99ed3a2
igor atakhanov <igoratakhanov@gmail.com>
2017-12-05 23:29:02 -0500

    Basic functionality complete
    What's left: testing framework, native DB, authentication

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,8 +2,13 @@ var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 
 // connect to db
-mongoose.connect('mongodb://test:test@ds131826.mlab.com:31826/todo')
-
+//mongoose.connect('mongodb://test:test@ds131826.mlab.com:31826/todo')
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://test:test@ds131826.mlab.com:31826/todo', {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
+  useMongoClient: true
+});
 // create scheme
 var todoSchema = new mongoose.Schema({
   item: String
@@ -11,23 +16,35 @@ var todoSchema = new mongoose.Schema({
 
 var Todo = mongoose.model('Todo', todoSchema)
 
-var data =[{item: 'get milk'}, {item: 'get catfood'}, {item: 'shine shoes'}]
+
+
+//var data =[{item: 'get milk'}, {item: 'get catfood'}, {item: 'shine shoes'}]
 
 var urlencodedParser = bodyParser.urlencoded({extended: false})
 
 module.exports = function(app){
 
   app.get('/todo', function(req, res){
-    res.render('todo', {todos: data})
+    // get data from mongodb and pass to view
+    Todo.find({}, function(err, data){
+      if(err) throw err
+      res.render('todo', {todos: data})
+    })
   })
 
   app.post('/todo', urlencodedParser, function(req, res){
-    data.push(req.body);
-    console.log(data)
-    res.json(data)
+    // get data from view and pass it to mongodb
+    var newTodo = Todo(req.body).save(function(err, data){
+      if(err) throw err;
+      res.json(data)
+    })
   })
 
-  app.delete('/todo', function(req, res){
-
+  app.delete('/todo/:item', function(req, res){
+    // deete requrested item from mongodb
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+      if(err) throw err
+      res.json(data)
+    })
   })
 }
